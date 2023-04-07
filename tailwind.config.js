8f299d3cb3c0a83bc2b167d8a481d7336c200c5b
safiullah77777 <safiullahrafeeq7777@gmail.com>
2023-04-07 16:44:55 +0500

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -49,7 +49,7 @@ module.exports = {
 
       backgroundColor: {
         // primary: "#F6F9FC",
-        primary: "#eef2f6",
+        primary: "#F3F8FA",
       },
       colors: {
         primaryBlack: "#160042",
@@ -58,7 +58,7 @@ module.exports = {
         tertiaryGray: "#B6B9CE",
         heading: "#160042",
         // border:'#efe9ff'
-        border:'#DDE5EE'
+        border:'#E4EBF1'
       },
       boxShadow: {
         cardShadow: "51px 51px 132px -16px rgb(49 51 114 / 8%)",
