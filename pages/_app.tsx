0145dcd5423ebcadf5df95988353ced00b218cbe
safiullah77777 <safiullahrafeeq7777@gmail.com>
2023-04-11 13:52:51 +0500

    changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import "../styles/globals.css";
 import { AppProps } from "next/app";
 import { Router, useRouter } from "next/router";
 import { useDispatch, useSelector } from "react-redux";
-import { setIsAnimating } from "../store/slices/featues";
+import { setIsAnimating, updateMenu } from "../store/slices/featues";
 import { useEffect, useRef } from "react";
 import { Progress } from "../components/progress/Progress";
 import Sticker1 from "../components/Sticker/Sticker1";
@@ -34,7 +34,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     const handleStop = () => {
       dispatch(setIsAnimating(false));
     };
-
+    dispatch(updateMenu(false))
     router1.events.on("routeChangeStart", handleStart);
     router1.events.on("routeChangeComplete", handleStop);
     router1.events.on("routeChangeError", handleStop);
