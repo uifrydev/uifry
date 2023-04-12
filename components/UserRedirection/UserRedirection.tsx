092ee94e90f9e19c490123a23b11e4810de79cba
safiullah77777 <safiullahrafeeq7777@gmail.com>
2023-04-12 17:44:32 +0500

    changes

--- components/UserRedirection/UserRedirection.tsx
+++ components/UserRedirection/UserRedirection.tsx
@@ -3,25 +3,25 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { NextPage } from "next";
 
-const withRedirectIfUserPresent = <P,>(
-  WrappedComponent: NextPage<P>
-): NextPage<P> => {
-  const Wrapper: NextPage<P> = (props: P) => {
-    const [loading, setLoading] = useState(true);
-    const router = useRouter();
-    useEffect(() => {
-      const userIsPresent = !!localStorage.getItem("token");
-      // Replace this condition with your own user check logic
-      if (userIsPresent) {
-        router.back();
-      }
-      setLoading(false);
-    }, [router]);
-    if (loading) return <></>;
-    return <WrappedComponent {...props} />;
-  };
+// const withRedirectIfUserPresent = <P,>(
+//   WrappedComponent: NextPage<P>
+// ): NextPage => {
+//   const Wrapper: NextPage<P> = (props: P) => {
+//     const [loading, setLoading] = useState(true);
+//     const router = useRouter();
+//     useEffect(() => {
+//       const userIsPresent = !!localStorage.getItem("token");
+//       // Replace this condition with your own user check logic
+//       if (userIsPresent) {
+//         router.back();
+//       }
+//       setLoading(false);
+//     }, [router]);
+//     if (loading) return <></>;
+//     return <WrappedComponent {...props} />;
+//   };
 
-  return Wrapper;
-};
-
+//   // return Wrapper;
+// };
+const withRedirectIfUserPresent=()=>{}
 export default withRedirectIfUserPresent;
