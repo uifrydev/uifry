31aef4059aa708ddd48e4d775df8b679532dbd06
safiullah77777 <safiullahrafeeq7777@gmail.com>
2023-04-14 16:03:00 +0500

    changes

---
+++ components/JobCard/SkeletonJobCard.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const SkeletonJobCard = () => {
+  return (
+    <div className="p-[3rem] flex flex-col gap-[1.6rem] rounded-[1.2rem] bg-[#fff]">
+    <div className="flex justify-between items-center">
+      <div className="w-[5.4rem] h-[5.4rem] rounded-full bg-gray-300 animate-pulse"></div>
+      <div className="flex flex-col gap-[.4rem]">
+        <div className="h-[1.2rem] bg-gray-300 rounded-full animate-pulse"></div>
+        <div className="h-[1.4rem] w-[8rem] bg-gray-300 rounded-full animate-pulse"></div>
+      </div>
+    </div>
+    <div className="h-[1.8rem] w-[10rem] bg-gray-300 rounded-full animate-pulse"></div>
+    <div className="h-[6rem] bg-gray-300 rounded-lg animate-pulse"></div>
+    <div className="flex gap-[1.3rem] flex-col">
+      <div className="h-[1.8rem] w-[8rem] bg-gray-300 rounded-full animate-pulse"></div>
+      <div className="flex gap-[2rem]">
+        <div className="flex gap-[0.7rem]">
+          <div className="w-[1.4rem] h-[1.4rem] rounded-full bg-gray-300 animate-pulse"></div>
+          <div className="h-[1.4rem] w-[5rem] bg-gray-300 rounded-full animate-pulse"></div>
+        </div>
+        <div className="flex gap-[0.7rem]">
+          <div className="w-[1.4rem] h-[1.4rem] rounded-full bg-gray-300 animate-pulse"></div>
+          <div className="h-[1.4rem] w-[5rem] bg-gray-300 rounded-full animate-pulse"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default SkeletonJobCard
--- pages/jobs/index.tsx
+++ pages/jobs/index.tsx
@@ -1,3 +1,4 @@
+import SkeletonJobCard from "@/components/JobCard/SkeletonJobCard";
 import { Data, JobProps } from "@/Interface/interface";
 import { NextPage } from "next";
 import Link from "next/link";
@@ -20,6 +21,7 @@ const Jobs: NextPage<{
       <JobsFilterBar initialData={Jobs} setProducts={setProducts} />
       <div className="min-lg:pl-[234px] lg:px-[1rem]  pr-[4rem] pt-[2rem] w-full ">
         <div className=" grid grid-cols-3 3xl:grid-cols-2 xs1:grid-cols-1 bg-primary rounded-[2.4rem] gap-[3rem] xs:px-[1rem] p-[3rem]">
+          <SkeletonJobCard />
           {products.map((item, index) => (
             <Link
               key={index}
