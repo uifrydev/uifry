8e367ba88469adb57e1119dcf17237f832d893cd
safiullah77777 <safiullahrafeeq7777@gmail.com>
2023-04-17 21:01:20 +0500

    changes

---
+++ pages/briefs/details/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BriefDetails = () => {
+  return (
+    <div>BriefDetails</div>
+  )
+}
+
+export default BriefDetails
--- pages/briefs/index.tsx
+++ pages/briefs/index.tsx
@@ -9,42 +9,26 @@ import { RootState } from "@/store/store";
 import { MainCardProps } from "@/Interface/interface";
 import { breifList, list } from "@/utils/links";
 import Link from "next/link";
-import { updateBriefModal } from "@/store/slices/featues";
-import BriefModal from "@/components/DetailsModal/BreifModal";
 const Briefs = () => {
-  const { openModal, briefModal } = useSelector(
-    (state: RootState) => state.features
-  );
+  const openModal = useSelector((state: RootState) => state.features.openModal);
   const dispatch = useDispatch();
 
   return (
     <>
-      {BriefModal && <BriefModal />}
+      {/* {openModal && <DetailsModal  />} */}
       <Header title={["Briefs"]} breadcrums={["Briefs"]} />
       <Sidebar isDetail={false} />
       {/* <FilterBar /> */}
       <div className="min-lg:pl-[234px] lg:px-[1rem]  pr-[4rem] pt-[2rem] w-full ">
         <div className=" grid grid-cols-3 lg1:grid-cols-2 xs1:grid-cols-1 bg-primary rounded-[2.4rem] gap-[3rem] p-[3rem]">
           {breifList.map((item, index) => (
-            <Link
-              href={{ pathname: `/briefs/${item.link}` }}
-              onClick={(e) => e.preventDefault()}
-              key={index}
-            >
-              <a
-                onClick={() => {
-                  updateBriefModal(true);
-                  window.scrollBy(0, 2);
-                  document.body.classList.add("!overflow-y-hidden");
-                }}
-              >
-                <MainCard
-                  desc={item.desc}
-                  img={item.img}
-                  includes={item.includes}
-                  title={item.title}
-                />
-              </a>
+            <Link href={{ pathname: `/briefs/${item.link}` }} key={index}>
+              <MainCard
+                desc={item.desc}
+                img={item.img}
+                includes={item.includes}
+                title={item.title}
+              />
             </Link>
           ))}
         </div>
