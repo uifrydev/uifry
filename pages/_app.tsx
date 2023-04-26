5d93c34ffaff3d10c2b673862ff1f176d0d6d392
safiullah77777 <safiullahrafeeq7777@gmail.com>
2023-04-26 13:06:09 +0500

    changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,7 +33,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     const isProduction = process.env.NODE_ENV === 'production';
     const specificDomain = 'uifry.com';
-
+    console.log(window.location.hostname)
     if (isProduction && window.location.hostname === specificDomain) {
       setShowComingSoon(true);
       alert('ssa')
