fb84d1b40b312b5227e34ada27606b6375385866
safiullah77777 <safiullahrafeeq7777@gmail.com>
2023-04-28 18:30:40 +0500

    changes

--- pages/jobs/index.tsx
+++ pages/jobs/index.tsx
@@ -8,7 +8,8 @@ import JobCard from "../../components/JobCard/JobCard";
 import JobsFilterBar from "../../components/JobsFilterBar/JobsFilterBar";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import sanity from "../../sanity";
-import { fetchDataServer } from "../../utils/functions";
+import { fetchData, fetchDataServer } from "../../utils/functions";
+import Button from "@/components/Button/Button";
 
 const Jobs: NextPage<{
   Jobs: JobProps[];
@@ -34,27 +35,83 @@ const Jobs: NextPage<{
         setProductIndex={setProductIndex}
         isLoading={isLoading}
       />
-      <div className="min-lg:pl-[234px] lg:px-[1rem]  pr-[4rem] pt-[2rem] w-full ">
-        <div className=" grid grid-cols-3 3xl:grid-cols-2 xs1:grid-cols-1 bg-primary rounded-[2.4rem] gap-[3rem] xs:px-[1rem] p-[3rem]">
-          {isLoading &&
-            Array.from({ length: 12 }).map((_, index) => (
-              <SkeletonJobCard key={index} />
+      <div className="min-lg:pl-[234px] lg:px-[1rem]  pr-[4rem] pt-[2rem]  w-full ">
+        <div className="bg-primary rounded-[2.4rem] flex flex-col gap-[2rem]">
+          <div className=" grid grid-cols-3 3xl:grid-cols-2 xs1:grid-cols-1 gap-[3rem] xs:px-[1rem] p-[3rem]">
+            {isLoading &&
+              Array.from({ length: 12 }).map((_, index) => (
+                <SkeletonJobCard key={index} />
+              ))}
+            {products.map((item, index) => (
+              <Link
+                key={index}
+                href={{
+                  pathname: "jobs/details",
+                  query: { job: item.slug.current },
+                }}
+              >
+                <JobCard data={item} />
+              </Link>
             ))}
-          {products.map((item, index) => (
-            <Link
-              key={index}
-              href={{
-                pathname: "jobs/details",
-                query: { job: item.slug.current },
-              }}
-            >
-              <JobCard data={item} />
-            </Link>
-          ))}
-          {isLoadmoreLoading &&
-            Array.from({ length: 12 }).map((_, index) => (
-              <SkeletonJobCard key={index} />
-            ))}
+            {isLoadmoreLoading &&
+              Array.from({ length: 12 }).map((_, index) => (
+                <SkeletonJobCard key={index} />
+              ))}
+          </div>
+          <Button
+            onClick={async () => {
+              console.log(`*[_type=='job'${
+                filter.subCategory !== "All Jobs"
+                  ? ` && subCategory=='${filter.subCategory}'`
+                  : ""
+              }${
+                filter.type !== "All" ? ` && jobType=='${filter.type}'` : ""
+              } && applyBefore >= now()]{
+                    body,
+                    companyName,
+                    salaryRange,
+                    title,
+                    slug,
+                    description,
+                    ,
+                    jobType,
+                     priimagesmaryIndustry,
+                    tags,foundedIn,companySize,
+                      subCategory,jobPosted,applyBefore
+              }`)
+              setProducts([]);
+              await fetchData({
+                isLoading: isLoadmoreLoading,
+                setLoading: setLoadmoreLoading,
+                setProductIndex,
+                setCards: setProducts,
+                sanity,
+                query: `*[_type=='job'${
+                  filter.subCategory !== "All Jobs"
+                    ? ` && subCategory=='${filter.subCategory}'`
+                    : ""
+                }${
+                  filter.type !== "All" ? ` && jobType=='${filter.type}'` : ""
+                } && applyBefore >= now()]{
+                      body,
+                      companyName,
+                      salaryRange,
+                      title,
+                      slug,
+                      description,
+                      ,
+                      jobType,
+                       priimagesmaryIndustry,
+                      tags,foundedIn,companySize,
+                        subCategory,jobPosted,applyBefore
+                }`,
+              });
+            }}
+          >
+            <span className="satoshi text-[1.6rem] font-500 text-[#F7F8FD] rounded-[3.2rem] px-[2.4rem] py-[1.2rem] bg-gradient">
+              {isLoading ? "Loading..." : "Load More"}
+            </span>
+          </Button>
         </div>
       </div>
     </>
