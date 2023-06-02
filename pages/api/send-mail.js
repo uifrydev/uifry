fefaa6a1e0fc184805f8e2cd96a2ec75b2884a32
safiullah77777 <safiullahrafeeq7777@gmail.com>
2023-06-02 19:05:59 +0500

    changes

--- pages/api/send-mail.js
+++ pages/api/send-mail.js
@@ -2,7 +2,8 @@ import { nodeMailer } from "../../utils/nodemailer";
 export default async function handler(req, res) {
   if (req.method !== "POST")
     return res.status(404).send({ message: "Route Not Found" });
-  const node_mailer = await nodeMailer();
+  const { name, email, comments, link } = req.body;
+  const node_mailer = await nodeMailer({name, email, comments, link});
   console.log({ node_mailer });
   res.status(200).json({
     message: "Hello World",
--- utils/nodemailer.ts
+++ utils/nodemailer.ts
@@ -1,5 +1,15 @@
 import nodemailer from "nodemailer";
-export const nodeMailer = () => {
+export const nodeMailer = ({
+  name,
+  email,
+  link,
+  comments,
+}: {
+  name: string;
+  email: string;
+  link: string;
+  comments: string;
+}) => {
   let transporter = nodemailer.createTransport({
     service: "gmail",
     host: "smtp.gmail.com",
@@ -15,10 +25,25 @@ export const nodeMailer = () => {
     from: "safiullah.xeon@gmail.com",
     // to: "kashaf.creaditech@gmail.com",
     to: "safiullah.xeon@gmail.com",
-    subject: "options.subject",
-    html: `<div class"text-[10rem] text-[#000]">
-    <h1>Email Sent</h1>
-  </div>`,
+    subject: "Brief Sent by User.",
+    html: `<div style="">
+      <div style="display:flex;gap:10px;">
+      <span style="font-weight:700;color:#000000;">User Name: </span>
+      <span style="font-weight:400;color:grey;">${name}</span>
+      </div>
+      <div style="display:flex;gap:10px;">
+      <span style="font-weight:700;color:#000000;">User Email: </span>
+      <span style="font-weight:400;color:grey;">${email}</span>
+      </div>
+      <div style="display:flex;gap:10px;">
+      <span style="font-weight:700;color:#000000;">User Comments: </span>
+      <span style="font-weight:400;color:grey;">${comments}</span>
+      </div>
+      <div style="display:flex;gap:10px;">
+      <span style="font-weight:700;color:#000000;">Figma/Google Drive Link: </span>
+      <a href="${link}" style="">${link}</a>
+      </div>
+    </div>`,
     // attachments:[...options.attachments]
   });
 };
