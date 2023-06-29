216ec91fef72d2edf9ccbfec2f4ea84a8204bf2e
safiullah77777 <safiullahrafeeq7777@gmail.com>
2023-06-29 23:11:17 +0500

    djd

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -71,6 +71,18 @@ var o_options = {
         <Script strategy="lazyOnload">
           {`!function(w, d, t, u) { if (w.__ls) return; var f = w.__ls = function() { f.push ? f.push.apply(f, arguments) : f.store.push(arguments)}; if (!w.__ls) w.__ls = f; f.store = []; f.v = "1.0"; var ls = d.createElement(t); ls.async = true; ls.src = u; var s = d.getElementsByTagName(t)[0]; s.parentNode.insertBefore(ls, s); }(window, document, 'script', ('https:' == window.location.protocol ? 'https://' : 'http://') + 'cdn.livesession.io/track.js'); __ls("init", "d58bb084.155e2e0b"); __ls("newPageView") `}
         </Script>
+
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-11239692819"
+        ></Script>
+        <Script strategy="lazyOnload">
+          {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-11239692819');`}
+        </Script>
       </Head>
       <body>
         <Main />
